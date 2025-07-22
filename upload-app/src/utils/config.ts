954035c450700ac66ff2702df4088a44a9e49fd6
Charlie Morris <cshotam@gmail.com>
2025-07-22 14:08:53 +1000

    switch from local uploads folder to shared volume provider by docker

--- upload-app/src/utils/config.ts
+++ upload-app/src/utils/config.ts
@@ -1,8 +1,8 @@
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+// import path, { dirname } from "path";
+// import { fileURLToPath } from "url";
 
 // for local testing look for the uploads dir on the root of app
-const __dirname = dirname(fileURLToPath(import.meta.url));
-export const uploadsDir = path.resolve(__dirname, "../../uploads");
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+// export const uploadsDir = path.resolve(__dirname, "../../uploads");
 
-// export const uploadsDir = "/uploads";
+export const uploadsDir = "/uploads";
