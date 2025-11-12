ec253ce97cfb16e96a2824f72fb977345cf4d837
Charlie Morris <cshotam@gmail.com>
2025-11-12 14:45:32 +1100

    add rm volume at the start for debuging

--- local-testing.sh
+++ local-testing.sh
@@ -7,6 +7,9 @@ COMPOSE_FILE="docker-compose.yml" # Use the main docker-compose.yml file
 echo "--- Removing existing stack (if any) ---"
 docker stack rm "$STACK_NAME" 2>/dev/null || true
 
+echo "rm go app volumne"
+docker volume rm -f local-app-stack_go-app
+
 # Wait until all services from the stack are gone
 echo "Waiting for stack '$STACK_NAME' to fully remove..."
 while docker stack ls | grep -q "$STACK_NAME"; do
